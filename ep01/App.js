910fc25fc74115b9aa5fc45a15753fd436453cdb
Chehak Sharma <chehak@192.168.1.17>
2024-11-27 00:39:00 +0530

    ep03

--- ep01/App.js
+++ ep01/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", 
-{ id: "heading" }, 
-"Namaste React 🚀");
+const elem = <span>ReactElement</span>
 
-const jsxHeading
+const Title = () => (
+    <h1 className="head" tabIndex="5">    
+        Namaste React
+    </h1>
+); 
+
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        <h1 className="heading">Func Component</h1>
+    </div>
+); 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent/>);
