fdc082bb7bb75ea9852e105e1c4893e3f5f4b058
Chehak Sharma <ch3hak@gmail.com>
2024-11-30 19:21:54 +0530

    make filter for rating

--- ep01/src/components/Body.js
+++ ep01/src/components/Body.js
@@ -1,17 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 
 const Body = () => {
 
-const listOfRestaurants = [];
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 
     return (
+        // react virtual dom 
         <div className="body">
             <div className="filter">
                 <button 
                     className="filter-btn" 
                     onClick={() => {
-                        console.log("button clicked");
+                        const filteredList = listOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                        setListOfRestaurants(filteredList);
                     }}
                 >
                     Top Rated Restaurants
@@ -21,7 +27,7 @@ const listOfRestaurants = [];
                 {listOfRestaurants.map((restaurant => 
                     <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                 ))
-                };
+                }
                 
             </div>
         </div>
